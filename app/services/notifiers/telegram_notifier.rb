d693a1a7fe09b7d78b9be24cac5e258798959d4d
Alexey Tomilov <apofeosz@yandex.ru>
2025-07-26 19:47:12 +0500

    TelegramChannel: WIP v1.3 [skip-ci]

--- app/services/notifiers/telegram_notifier.rb
+++ app/services/notifiers/telegram_notifier.rb
@@ -2,7 +2,17 @@ class Notifiers::TelegramNotifier
   def initialize(channel) = @channel = channel
 
   def call(alert, price)
-    # TODO: Implement telegram notification logic
-    Rails.logger.info "Sending telegram notification to #{@channel.chat_id} for #{alert.symbol} at #{price}"
+    token = ENV.fetch("TELEGRAM_BOT_TOKEN")
+    url = "https://api.telegram.org/bot#{token}/sendMessage"
+    message = "Price alert for #{alert.symbol}: the price has reached #{price}"
+
+    response = Faraday.post(url, {
+      chat_id: @channel.chat_id,
+      text: message
+    }.to_json, "Content-Type" => "application/json")
+
+    unless response.success?
+      Rails.logger.error "Failed to send Telegram notification: #{response.body}"
+    end
   end
 end
