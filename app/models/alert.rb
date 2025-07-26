bc17f84cc310d4108912e2668fd41ef27cabb534
Alexey Tomilov <apofeosz@yandex.ru>
2025-07-26 16:47:27 +0500

    WIP v1.2 [skip-ci]

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,2 +1,6 @@
 class Alert < ApplicationRecord
+  enum :side, %w[above below].index_by(&:itself)
+
+  validates :symbol, inclusion: { in: %w[BTCUSDT ETHUSDT BNBUSDT] }
+  validates :price,  numericality: { greater_than: 0 }
 end
